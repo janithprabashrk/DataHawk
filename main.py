4dc1a8cb9f385e1fbbc31e250b8e97966035f04b
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-07-06 16:28:43 +0530

    [DH 003] FEAT : Enhance data fetching and display in main.py
    Added extraction and cleaning of body content after scraping the website. The cleaned DOM content is now stored in session state and displayed in an expandable section for better user experience.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    extract_body_content,
+    clean_body_content
+)
 
 
 st.title("DataHawk")
@@ -8,5 +13,13 @@ url = st.text_input("Enter Website URL : ")
 
 if st.button("Fetch Data"):
     st.write(f"Fetching data from {url}...")
+
     result = scrape_website(url)
-    print(result)
+
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.write("DOM Content", cleaned_content, height=300)
