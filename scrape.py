2202ddc73b9d8bca9e79a5d1190aaea5b268db8d
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-07-06 16:05:04 +0530

    [DH 002] FEAT : Refactor scrape_website to use SuperBrowser Remote WebDriver
    Updated the scrape_website function to connect via the SuperBrowser Remote WebDriver service using ChromiumRemoteConnection. Added CAPTCHA handling logic and replaced the local ChromeDriver setup with a remote connection. The function now navigates to a hardcoded example URL and prints the page source.

--- scrape.py
+++ scrape.py
@@ -3,20 +3,27 @@ from selenium.webdriver.chrome.service import Service
 import time
 
 
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+
+SBR_WEBDRIVER = 'https://brd-customer-hl_815e11a3-zone-datahawk:t2v1xurwlh62@brd.superproxy.io:9515'
+# This is the URL for the SuperBrowser Remote WebDriver service.
+
+
 def scrape_website(website):
     print("Launching browser... Please wait.")
 
-    chrome_driver_path = "./chromedriver.exe"
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
-        driver.get(website)
-        print(f"Page loaded...")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating to https://example.com...')
+        driver.get('https://example.com')
+        # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Browser API's automatic CAPTCHA solver
+        print('Waiting captcha to solve...')
+        solve_res = driver.execute('executeCdpCommand', {
+            'cmd': 'Captcha.waitForSolve',
+            'params': {'detectTimeout': 10000},
+        })
+        print('Captcha solve status:', solve_res['value']['status'])
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        time.sleep(2)  # Wait for the page to load completely
-
-        return html
-    
-    finally:
-        driver.quit()
+        print(html)
